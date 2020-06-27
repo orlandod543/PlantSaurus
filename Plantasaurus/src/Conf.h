905c3b276f2b454c7fb742e669322217e95a8239
Orlando Diaz <orlandod543@gmail.com>
2020-06-27 11:56:47 +0200

    git commit -m "Ready example"

--- Plantasaurus/src/Conf.h
+++ Plantasaurus/src/Conf.h
@@ -1,8 +1,12 @@
 #ifndef CONF_H
 #define CONF_H
 
-#define SSIDName "Ziggo5320622"
-#define SSIDPasswd "yf5MmsfbB9fv"
+#define SSIDName "Koraalstraat28A"
+#define SSIDPassword "Nami_488"
+#define SSIDSecurity NSAPI_SECURITY_WPA2
+
+#define TARDISIP "192.168.178.19"
+#define TARDISPORT 1111
 
 #define MOISTUREPIN A0
 #define DRYCAL 7864
@@ -23,8 +27,8 @@
 #define ESP8266_BAUD 115200
 //#define SETRTC true
 
-#define MBED_CONF_APP_WIFI_SSID "Koraalstraat28A"
-#define MBED_CONF_APP_WIFI_PASSWORD "Nami_488"
+#define MBED_CONF_APP_WIFI_SSID SSIDName
+#define MBED_CONF_APP_WIFI_PASSWORD SSIDPasswd
 #define MBED_CONF_ESP8266_TX D14
 #define MBED_CONF_ESP8266_RX D15
 #endif
